447560c19fc3e2ed3546f7b89f8ce0e73d0b33e6
Tobias Hartwich <mail@tha.io>
2015-02-21 09:46:01 +0100

    completed MPD search model: send search query to MPD and result back to the browser

--- src/model/search.js
+++ src/model/search.js
@@ -9,6 +9,25 @@ var Search = function() {
 }
 util.inherits(Search, events.EventEmitter);
 
+Search.prototype.search = function(query, callback) {
+	  app.mpd.sendCommand(cmd("search", ["any", '"'+query+'"']), function(err, msg) {
+      if (err) throw err;
 
+      var songs = [];
+      splitted = msg.split("file: ");
+      splitted.forEach(function(value) {
+        if (!value) return;
+
+        var playlistsong = new Song();
+        playlistsong.fromPlaylistFile(value);
+        songs.push(playlistsong);
+
+      });
+
+      callback({
+        result: songs
+      });
+  });
+}
 
 module.exports = new Search();
