cbe2aa8ba5cd91e95454f9a6dada8cc16bec80de
Tobias Hartwich <mail@tha.io>
2015-03-04 02:18:41 +0100

    extendes wishlist model: adding admin functions to accept/reject wish

--- src/model/wishlist.js
+++ src/model/wishlist.js
@@ -7,11 +7,11 @@ var mpd = require('mpd'),
 var Whishlist = function() {
   var self = this;
   events.EventEmitter.call(this);
-	this.globalWishlist = [];
-	this.minutesToClean = 20;
+  this.globalWishlist = [];
+  this.minutesToClean = 20;
 
-	// Regelmäßig alte bearbeitete Wünsche entfernen
-	this.cleaningInterval = setInterval(function() { self.cleanWishlist(); }, 60*1000);
+  // Regelmäßig alte bearbeitete Wünsche entfernen
+  this.cleaningInterval = setInterval(function() { self.cleanWishlist(); }, 60*1000);
 }
 util.inherits(Whishlist, events.EventEmitter);
 
@@ -64,6 +64,101 @@ Whishlist.prototype.generateGlobalWishlist = function() {
   });
 }
 
+Whishlist.prototype.acceptRefuseSong = function(file, pos, accept, reason) {
+  var self = this;
+  // Ist der Song in der Wishlist?
+  var listmatch = _.filter (this.globalWishlist, function(wish) {
+      return wish.filename === file;
+  });
+  if (listmatch.length > 0) {
+    // Ist drin
+    var wish = listmatch[0];
+    // wishlist des users der sich das gewünscht hat holen
+    app.redis.hget("wishlist", wish.user ,function (err, userwishlist) {
+      if (err) throw new Error(err);
+      userwishlist = JSON.parse(userwishlist).wishes;
+
+      // Wunsch in der Liste finden
+      var userlistmatch = _.filter (userwishlist, function(uw) {
+          return uw.filename === file;
+      });
+      if (userlistmatch.length > 0) {
+        var userwish = userlistmatch[0];
+        if (accept) {
+          userwish.accepted = true;
+        }
+        else {
+          userwish.accepted = false;
+          userwish.reason = reason
+        }
+        userwish.acceptTime = new Date().getTime();
+
+        app.redis.hset("wishlist", wish.user, JSON.stringify({ wishes: userwishlist }), function (err) {
+          if (err) throw new Error(err);
+          // Globale Liste neu laden
+          self.generateGlobalWishlist();
+
+          if (accept) {
+            if (pos < 0) {
+              app.mpd.sendCommand(cmd("add", ['"'+file+'"', ]), function(err, msg) {
+                if (err) throw err;
+
+              });
+            }
+            else {
+              app.mpd.sendCommand(cmd("addid", ['"'+file+'"', pos]), function(err, msg) {
+                if (err) throw err;
+
+              });
+            }
+          }
+
+        });
+
+      }
+    });
+
+
+  }
+
+}
+
+Whishlist.prototype.cleanWishlist = function(file, pos, accept, reason) {
+  var self = this;
+   this.globalWishlist.forEach(function(wish) {
+     if (wish.acceptTime) {
+       var now = new Date().getTime();
+       var tdiff = parseInt((now - wish.acceptTime) / 1000 / 60);
+       console.log("tdiff: " + tdiff);
+       if (tdiff >= self.minutesToClean) {
+         self._deleteWish(wish);
+         console.log("Deletewish");
+       }
+     }
+   });
+}
+
+Whishlist.prototype._deleteWish = function(wish) {
+  var self = this;
+  // wishlist holen
+  app.redis.hget("wishlist", wish.user ,function (err, userwishlist) {
+    if (err) throw new Error(err);
+
+    userwishlist = JSON.parse(userwishlist);
+    var newwishlist = [];
+    userwishlist.wishes.forEach(function(song) {
+      if (song.filename !== wish.filename) {
+        newwishlist.push(song);
+      }
+    });
+    app.redis.hset("wishlist", wish.user , JSON.stringify({wishes: newwishlist}) ,function (err, userwishlist) {
+      if (err) throw new Error(err);
+      self.generateGlobalWishlist();
+    });
+
+  });
+
+}
 
 
 module.exports = new Whishlist();
