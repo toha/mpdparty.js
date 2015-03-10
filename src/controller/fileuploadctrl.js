01a4af1e7f1eefef5cf9e91a672daa24f9545c4d
Tobias Hartwich <mail@tha.io>
2015-03-10 22:07:31 +0100

    finished FileUploadCtrl queue; resizing uploaded photos

--- src/controller/fileuploadctrl.js
+++ src/controller/fileuploadctrl.js
@@ -26,5 +26,63 @@ FileUploadCtrl.prototype.processUpload = function(fileobj) {
   this.queue.add(this.queueRunner, JSON.stringify(fileobj));
 }
 
+FileUploadCtrl.prototype.queueRunner = function(job) {
+  console.log("queueRunner");
+    var err;
+    var fileobj = JSON.parse(job.name);
+
+    // Prüfen ob es die Datei gibt
+    fs.exists(fileobj.tmpname, function(exists) {
+      if (exists) {
+
+        // Versuchen Das Bild zu verkleinern
+        var resizeOptions = {
+          srcPath: fileobj.tmpname,
+          dstPath: app.uploadFilePath + "big/" + fileobj.filename,
+          quality: 0.8,
+          format: 'jpg',
+          width: 1024
+        }
+
+        im.resize(resizeOptions, function(err, stdout, stderr){
+          if (err)  {
+            job.finish(err);
+          }
+
+          var resizeOptionsThumb = {
+            srcPath: fileobj.tmpname,
+            dstPath: app.uploadFilePath + "thumb/" + fileobj.filename,
+            quality: 0.8,
+            format: 'jpg',
+            width: 100
+          }
+
+          im.resize(resizeOptionsThumb, function(err, stdout, stderr){
+            if (err)  {
+              job.finish(err);
+            }
+
+            childprocess.spawn('mv', [fileobj.tmpname, app.uploadFilePath + "raw/" + fileobj.filename])
+
+            app.photowishlist.addWish(fileobj);
+
+
+            job.finish(null, fileobj);
+
+          });
+
+        });
+
+      }
+      else {
+        job.finish(err);
+      }
+    });
+
+}
+
+/*FileUploadCtrl.prototype.onTaskFinish = function(name) {
+
+}*/
 
 module.exports = new FileUploadCtrl();
